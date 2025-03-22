c1fa59387c4717439ef1843fbe171f2a296b772b
svsux <90853675+svsux@users.noreply.github.com>
2025-03-22 08:11:33 +0600

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,10 +1,14 @@
-// utils/api.js
 import axios from "axios";
 
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIyIiwianRpIjoiNzMwYjQ5ZjJlYzEyZGYxNjNmY2JlMDUyNmM3ZGYyYTFkM2VmODU5YzlkZjBjOWYxMDMwNGQ2NTNmNWZiNzc3YTBiOGQxZDNjNDYyZWQ4ODciLCJpYXQiOjE3NDIzMjM2OTYuOTgzNjA4LCJuYmYiOjE3NDIzMjM2OTYuOTgzNjExLCJleHAiOjE3NzM4NTk2OTYuOTgxNzMsInN1YiI6IjM4NDExODk3NzE3NzI4OSIsInNjb3BlcyI6W119.l0GsxcQnHSdABErKnw15OBaPbteX9ygNCkyJcROMHDSh2Hl1KGJ8h2p4MjotgdPYhTm1VZV4LwL8jnW1LqPI1joGaiQAh5np9zV4Ayxw7AqTVDPlTaAS0x6z6-uQO02nupukwoFnbjqY_LuKKBvi-aHuuyg592sKJxn_7JpkUJkpeJ03eV8cd8Pz0GYc1iyV2MVmim9fF6y8UONxPKIoWkBdpdYNmt53QuCv4mUpu2FuwSR3VGRZs9cP8y4oLP_8leWVWDywu20mdmYBpjMYoQg6GCzi5QpnLSxEY4YyKBtOENZ7xE2OxYu1sj9GBVwPk-1Ed__3y7YaZByehckICZSy24nXK4e5zZkDP0kJTr6I227f3ZCRwDFeSSo2UmaOOwOwd8KXBqnKtJFA7x5a2m8xGO81nsso0kbXN4ZDM6i7PAzQfrJWCkog2RNzuw65ljU4UroFyCwGqYsZq-uod8jCvEiAzRwOk8m6MZ3Zj_5ys88jSPYHUpTXodPKl3vCHqrX5RiGQq7skM9fhujTEe7CTnC9f_W_9EwYCAVUvwafGqpXBE0RSsAnj0lHl4fbgw7aRhO7UNY-9UP4KkysByMPFAIXpSWia9AkSzgzG0FQXIOX7ZMckGmmmqhy2YpBX8mSYpmkPSKQJkD9zgfsNUX7cfs0i7lpF_dvPlAC860";
+// Токен для iCafe API
+const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIyIiwianRpIjoiYjc3NDJjMzAwMDBkYWU1ZDAyYTUyOTdhNzVhYTgxMGZjMjQ0ZTcwM2QwNDI5YmUxZDE5OWJkODAyNmVjZDIyZDFlODkwNjExZDcxZjEwMTAiLCJpYXQiOjE3NDI2MDgzOTEuNDMzMzA1LCJuYmYiOjE3NDI2MDgzOTEuNDMzMzA4LCJleHAiOjE3NzQxNDQzOTEuNDMxMzQ4LCJzdWIiOiIzODQxMTg5NzcxNzcyODkiLCJzY29wZXMiOltdfQ.pJeKrg5EDdo6kYefWOkJ3ZnITNX0rW99GuCUzu5efqSXLSwlvyIFjLImnezJIvT_-zFHxUqAethpefHRMM-OxaaRHUaDPIjRdRQBb1nvzAk9yqjOXw0Ihl0ynefG6Yw8DFgWoJNHgl2FFYASzWuHo6PWCjyj1XRwThSPSV-KzYGuHG3GoVnlwi3S4hubClAFKGGUZ-l2l6w7H2QLQWWINakeyUFQ_I-OR9aNc22Uj1Tp4SOPHaPJkFWc5rYcN5MzdBi3pmEqFzEe9Py7cm6GZi4S19ZO1mI6EJcw9wsXWaSZ8JdDgg66IWRzzz-bWzVmWBmXGTU0-Mk_lC2eB6B2RNpW1ZXepBvRbKExapV4UJfIGCi62wfae8cwllb2p3wUs1sJlk8EliPdiGsJsJ0Zv5DDfvu9LRl0n4x3b7pe9XCErMKUHu368w1nBsiujd-iJOpN00uwTdqnThdD3_1H6sLOdZF93Bwh6yAyiC-i2uiXPQedRwD48LUb7dURD_Qr_r711PIq_gRWPQvUlwcXLBcmb9iA4dU0e8vV6QERzqV2CaQzCKlzHEoL63gSj4ufhEcL0n93ONpU_VK9se5YhNzqFuX1IhW4-l9UYwQqVWcjOoUnvQVI2JgAAL2bcsDihaOOEvQ_EMLGIk-M4RXbnTwi0BFuC_evr2EetJN3Up4";
+
+const PROXY_URL = "http://94.232.247.58:3001/proxy"; // URL твоего прокси
 
 export const fetchPCs = async () => {
-  const { data } = await axios.get("https://eu16.icafecloud.com/api/v2/cafe/77289/base/query", {
+  const { data } = await axios.post(PROXY_URL, {
+    endpoint: "/cafe/77289/base/query", // Правильный endpoint
+  }, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return data?.data?.pcs_init?.pc_list || [];
@@ -12,16 +16,22 @@ export const fetchPCs = async () => {
 
 export const bookPC = async (pc_name) => {
   await axios.post(
-    "https://eu16.icafecloud.com/api/v2/cafe/77289/pcs/action/setOutOfOrder",
-    { pcs: [{ pc_name, pc_enabled: 1 }] },
+    PROXY_URL,
+    {
+      endpoint: "/cafe/77289/pcs/action/setOutOfOrder",
+      body: { pcs: [{ pc_name, pc_enabled: 1 }] }
+    },
     { headers: { Authorization: `Bearer ${token}` } }
   );
 };
 
 export const unbookPC = async (pc_name) => {
   await axios.post(
-    "https://eu16.icafecloud.com/api/v2/cafe/77289/pcs/action/setOutOfOrder",
-    { pcs: [{ pc_name, pc_enabled: 0 }] },
+    PROXY_URL,
+    {
+      endpoint: "/cafe/77289/pcs/action/setOutOfOrder",
+      body: { pcs: [{ pc_name, pc_enabled: 0 }] }
+    },
     { headers: { Authorization: `Bearer ${token}` } }
   );
 };
